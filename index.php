ca070ebcf2e226c3b4292836714a8fae715fc893
fahmie <126230849+mingmie-002@users.noreply.github.com>
2023-09-05 16:11:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Index Form</title>
+</head>
+<body>
+
+<form class="profile" method="post" action="papar.php">
+    <h1>Tentang Diri Anda</h1>
+    <label>Ceritakan tentang diri anda:</label><br>
+    <textarea name="Pdiri" id="" cols="50" rows="10"></textarea><br>
+    <button type="submit">Simpan</button>
+</form>
+</body>
+</html>
